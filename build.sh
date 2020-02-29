91455200ef8edc355739f694a8872bc54850d38e
francois <frns.roy@gmail.com>
2020-02-29 16:06:28 -0500

    Added PETSc

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -eu pipefail #-euo
 mkdir -p build && cd build
 
 # Configure
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug \
+      -DPETSC_DIR=$HOME/build/dbeat/numerical/petsc \
+      -DPETSC_ARCH=arch-linux2-c-debug ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
 make intricacies
