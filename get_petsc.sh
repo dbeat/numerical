a4be8b4a06eaa0cc6fd54a882008f5c9f3719ff7
francois <frns.roy@gmail.com>
2020-02-29 16:10:33 -0500

    Added PETSc

--- get_petsc.sh
+++ get_petsc.sh
@@ -2,14 +2,14 @@ if [ -f petsc/README.md ]; then
   echo "PETSc directory exists."
   cd petsc
   make check
-  if [ $? -eq 0 ]; then
+  if [ $? -eq 99 ]; then
     echo "PETSc installed -- nothing to build."
     cd -
   else
     echo "Updating PETSc source."
     git pull
     echo "configuring and building PETSc"
-    ./configure # --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-mpich 
+    ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-mpich 
     make -j4
     make PETSC_DIR=$HOME/build/dbeat/numerical/petsc PETSC_ARCH=arch-linux2-c-debug check
     cd -
@@ -20,7 +20,7 @@ else
     git clone -b maint https://gitlab.com/petsc/petsc.git petsc
     echo "configuring and building mpich."
     cd petsc
-    ./configure # --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-mpich
+    ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-mpich
     make -j4
     make PETSC_DIR=$HOME/build/dbeat/numerical/petsc PETSC_ARCH=arch-linux2-c-debug check
     cd -
